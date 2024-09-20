7bced252638161e5f4bb939cc78adc419f601e5d
gad <nday.gady@gmail.com>
2024-09-19 20:07:53 -0700

    update

--- output.tf
+++ output.tf
@@ -1,9 +1,6 @@
 output "pip" {
   value = aws_instance.server1.public_ip
 }
-output "az" {
-  value = aws_instance.server1.availability_zone
-}
 output "ssh-command" {
   value = "ssh -i ec2-key.pem ec2-user@${aws_instance.server1.public_ip}"
 }
